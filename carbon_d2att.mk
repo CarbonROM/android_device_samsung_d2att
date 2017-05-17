6be8faea8d37ec9c08dfdb4556a7c2021063cc87
Joe Maples <joe@frap129.org>
2017-05-17 10:30:48 -0500

    d2att: Carbonize
    Signed-off-by: Joe Maples <joe@frap129.org>

--- carbon_d2att.mk
+++ carbon_d2att.mk
@@ -1,14 +1,16 @@
 $(call inherit-product, device/samsung/d2att/full_d2att.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common.mk)
+$(call inherit-product, vendor/carbon/config/gsm.mk)
+
+PRODUCT_NAME := carbon_d2att
+PRODUCT_DEVICE := d2att
+PRODUCT_MANUFACTURER := Samsung
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2uc \
     TARGET_DEVICE=d2att \
     BUILD_FINGERPRINT="samsung/d2uc/d2att:4.4.2/KOT49H/I747UCUFNJ2:user/release-keys" \
     PRIVATE_BUILD_DESC="d2uc-user 4.4.2 KOT49H I747UCUFNJ2 release-keys"
-
-PRODUCT_NAME := lineage_d2att
-PRODUCT_DEVICE := d2att
 
